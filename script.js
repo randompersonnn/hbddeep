080481150e3a6ef81b8650b781fda714873909b4
randompersonnn <72189947+randompersonnn@users.noreply.github.com>
2021-04-26 19:37:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-function myrangeFunction() {
-    let x = document.getElementById("range").value;
-    //   document.getElementById("demo").innerHTML = x;
-      console.log(x)
-    let y = x/100
-    document.getElementById("layer").style.opacity = y
+var slider = document.getElementById("range");
+var output = document.getElementById("ptag");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+//   let x = 1 -   this.value/100 
+  document.getElementById("layer").style.opacity = 1 - this.value/100
+
+  if(this.value == 100){
+      document.getElementById("layer").style.display = "none"
   }
+}
+
