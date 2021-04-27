e2f0fa95d2f6a7b3ec406a1d181a0f57bff2ec80
randompersonnn <72189947+randompersonnn@users.noreply.github.com>
2021-04-27 20:47:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,13 +2,23 @@ var slider = document.getElementById("range");
 var output = document.getElementById("ptag");
 output.innerHTML = slider.value;
 
-slider.oninput = function() {
+document.getElementById("range").oninput = function() {
   output.innerHTML = this.value;
 //   let x = 1 -   this.value/100 
-  document.getElementById("layer").style.opacity = 1 - this.value/100
+  document.getElementById("fstlayer").style.opacity = 1 - this.value/100
 
   if(this.value == 100){
-      document.getElementById("layer").style.display = "none"
+      document.getElementById("fstlayer").style.display = "none"
+  }
+}
+
+document.getElementById("secrange").oninput = function() {
+  output.innerHTML = this.value;
+//   let x = 1 -   this.value/100 
+  document.getElementById("seclayer").style.opacity = 1 - this.value/100
+
+  if(this.value == 100){
+      document.getElementById("seclayer").style.display = "none"
   }
 }
 
